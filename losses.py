44a7fd0bf6bcb8507613a4342dd62957433c70c4
SevenZhan <bin.zhan92@gmail.com>
2019-08-22 20:24:10 +0800

    create losses.py to hold frequent used loss functions for face recognition; add SV-X-Softmax Loss

---
+++ losses.py
@@ -0,0 +1,52 @@
+import torch
+from torch import nn
+
+class SVSoftmax(nn.Module):
+    """
+    An implementation of Support Vector Guided Softmax Loss for Face Recognition firstly proposed in this paper:
+    https://arxiv.org/pdf/1812.11317.pdf
+
+    Args:
+        embedding_size: feature dimension, e.g. 512
+        classnum: number of total classes
+        s: the scale value;
+            Default: 30
+        t: indicator parameter, see the paper for detailed introduction;
+            Default: 1.2
+        m: margin value used in Arcface Loss;
+            Default: 0.5
+    """
+    def __init__(self, embedding_size, classnum, s=30., t=1.2, m=0.5):
+        super(SVSoftmax, self).__init__()
+        # initial kernel
+        self.kernel = nn.Parameter(torch.empty(classnum, embedding_size))
+        nn.init.xavier_uniform_(self.kernel)
+
+        self.classnum = classnum
+        self.m = m
+        self.s = s
+        self.t = t
+
+    def forward(self, embbedings, label):
+
+        if not self.training:
+            self.m = 0.
+            self.t = 1
+
+        cos_m = math.cos(self.m)
+        sin_m = math.sin(self.m)
+
+        cos_theta = F.linear(F.normalize(embbedings), F.normalize(self.kernel))
+        sin_theta = torch.sqrt(1.0 - torch.pow(cos_theta, 2))
+        cos_theta_m = cos_theta * cos_m - sin_theta * sin_m
+        cos_theta_t = self.t * cos_theta + self.t - 1
+
+        # one hot encoding label
+        one_hot = torch.zeros_like(cos_theta).scatter_(1, label.view(-1, 1), 1)
+        # get predicted label from cos_theta
+        p_label = cos_theta[np.arange(cos_theta.size(0)), label].view(-1, 1)
+
+        cos_theta_m = torch.where(cos_theta > self.m, cos_theta_m, cos_theta)
+        output = (one_hot * cos_theta_m) + (1.0 - one_hot) * torch.where(((cos_theta > 0) & (cos_theta <= p_label)), cos_theta, cos_theta_t)
+
+        return output*self.s
